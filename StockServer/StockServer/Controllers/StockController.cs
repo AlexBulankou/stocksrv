6f5a91d6efa59a69f1781ca6bc48b6347bc183af
Alex Bulankou <bulankou@gmail.com>
2016-03-23 19:32:03 -0700

    Simple CORS server of stock info

---
+++ StockServer/StockServer/Controllers/StockController.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Helpers;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace StockServer.Controllers
+{
+    [Authorize]
+    public class StockController : ApiController
+    {
+       
+        // GET api/values/5
+        [Authorize]
+        [AllowAnonymous]
+        [HttpGet]
+        [EnableCors("*","*","*")]
+        public object Quote(string id)
+        {
+            using (var response = WebRequest.CreateHttp("https://finance.google.com/finance/info?client=ig&q=" + id).GetResponse())
+            {
+                using (var stream = response.GetResponseStream())
+                {
+                    using (var reader = new StreamReader(stream))
+                    {
+                        return JsonConvert.DeserializeObject(reader.ReadToEnd().Substring(4));
+                    }
+                }
+            }
+        }       
+    }
+}
